ff112020e778d69f554d42cc72d1e3b621ede06d
LaMinerva <joachimonyedikagi@gmail.com>
2025-12-02 16:37:51 +0100

    feat(CampaignActivity.java, activity_campaing.xml, ReadingTestIntroActivity.java, activity_reading_test_intro.xml): creati; update(AndroidManifest.xml)

---
+++ app/src/main/java/com/example/thereadingquest/ui/campaign/CampaignActivity.java
@@ -0,0 +1,20 @@
+package com.example.thereadingquest.ui.campaign;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.thereadingquest.R;
+
+public class CampaignActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_campaign);
+
+        TextView txt = findViewById(R.id.txtCampaignPlaceholder);
+        txt.setText("Sezione campagna");
+    }
+}
---
+++ app/src/main/java/com/example/thereadingquest/ui/test/ReadingTestIntroActivity.java
@@ -0,0 +1,20 @@
+package com.example.thereadingquest.ui.test;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.thereadingquest.R;
+
+public class ReadingTestIntroActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstance){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reading_test_intro);
+
+        TextView txt = findViewById(R.id.txtTestIntroPlaceholder);
+        txt.setText("Test di lettura");
+    }
+}
