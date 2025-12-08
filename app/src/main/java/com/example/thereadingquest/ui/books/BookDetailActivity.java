4d1cea261c42c8d6d6679c7494f74aa50a1cd8cf
LaMinerva <joachimonyedikagi@gmail.com>
2025-12-08 19:06:21 +0100

    todo(BookDetailActivity)

--- app/src/main/java/com/example/thereadingquest/ui/books/BookDetailActivity.java
+++ app/src/main/java/com/example/thereadingquest/ui/books/BookDetailActivity.java
@@ -17,13 +17,35 @@ import com.example.thereadingquest.ui.books.BookMissionActivity;
 
 public class BookDetailActivity extends AppCompatActivity {
 
-    private long BookId;
+    private long bookId;
     private DatabaseHelper dbHelper;
 
     private TextView txtTitolo, txtAutore, txtInfo;
     private Spinner spinnerStato;
     private Button btnSalvaStato, btnImpostaMissione;
 
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_book_detail);
 
+        dbHelper = new DatabaseHelper(this);
+
+        bookId = getIntent().getLongExtra("book_id", -1);
+        if(bookId == -1) {
+            Toast.makeText(this, "Errore: libro non valido", Toast.LENGTH_SHORT).show();
+            finish();
+            return;
+        }
+
+        txtTitolo = findViewById(R.id.txtTitoloLibro);
+        txtAutore = findViewById(R.id.txtAutoreLibro);
+        txtInfo = findViewById(R.id.txtInfoLibro);
+        spinnerStato = findViewById(R.id.spinnerStato);
+        btnSalvaStato = findViewById(R.id.btnSalvaStato);
+        btnImpostaMissione = findViewById(R.id.btnImpostaMissione);
+
+        Book libro = dbHelper.getLibroPerId(bookId)
+    }
 
 }
